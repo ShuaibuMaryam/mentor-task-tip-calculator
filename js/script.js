91caa823ebf9c67942cf6e66dc1d83aafa8aac0b
Pofi Abel <peterabel07@gmail.com>
2021-08-24 17:40:48 +0100

    fixed get selector

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-let
+const calculator = document.querySelector(".calculator");
+const tip_options = calculator.querySelector(".tip-options");
+
+tip_options.addEventListener('click', e => {
+ if (e.target.matches('button')) {
+   
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+
+    console.log(keyContent, action);
+     
+ }
+})
