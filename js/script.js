5b8cfd48df34c6840cb2f368a293b7760c89ebe1
ShuaibuMaryam <shuaibumaryam113@gmail.com>
2021-08-24 19:11:35 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-let
+// let bill = document.getElementById("bill").value;
+
+// let numberOfPeople = document.getElementById("people").value;
+
+
+// window.addEventListener('onload', function(event){
+//     event.preventDefault();
+
+//     let result = bill / numberOfPeople;
+//     document.getElementById("tip-amount").innerHTML = result;
+// })
