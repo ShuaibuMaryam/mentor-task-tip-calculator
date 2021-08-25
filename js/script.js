3bcb0cb7dab7b43ca66b4064bece8df880d7a051
ShuaibuMaryam <shuaibumaryam113@gmail.com>
2021-08-24 21:03:47 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const calculator = document.querySelector(".calculator");
 const tip_options = calculator.querySelector(".tip-options");
+con
 
 tip_options.addEventListener('click', e => {
  if (e.target.matches('button')) {
@@ -13,3 +14,7 @@ tip_options.addEventListener('click', e => {
  }
 });
 
+//Get the bill, take the percentage of the bill according to any of the buttons clicked ie, 15%, 20% etc. and divide it by
+//the number of people specified by the user. the result without dividing by number of people is displayed
+//just beside the text "tip amount" and the result after dividing is displayed beside the text "total / person"
+//by the right hand side... or at least that's what i think. sigh! hope i have not succeeded in confusing you.
