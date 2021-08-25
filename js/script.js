104b278447545157558ce01770c2fb4a3f18e68a
Pofi Abel <peterabel07@gmail.com>
2021-08-25 22:20:16 +0100

    update-fixed

--- js/script.js
+++ js/script.js
@@ -10,24 +10,18 @@ tip_options.addEventListener('click', ({ target }) => {
   if (target.matches('button')) {
     const action = target.dataset.action;
     // const keyContent = target.textContent;
-    // console.log(keyContent, action);
 
     const bill = get_bill.value;
     const numberPersons = get_people.value;
 
-      if (!get_bill.value && !get_bill.value) {
-        
-          console.log('Values needed!');
-          
-      } else if (isNaN(get_bill.value) && isNaN(get_bill.value)) {
-          
-          console.log("Invalid Input");
-          
-      } else {
-          
-          bill_calculation(action, bill, numberPersons);
-          
+    if (!get_bill.value && !get_bill.value) {
+      console.log('Values needed!');
+    } else if (isNaN(get_bill.value) && isNaN(get_bill.value)) {
+      console.log('Invalid Input');
+    } else {
+      bill_calculation(action, bill, numberPersons);
     }
+      
   }
 });
 
@@ -39,7 +33,6 @@ reset_button.addEventListener('click', () => {
 });
 
 /* METHODS */
-
 const bill_calculation = (x, y, z) => {
   let tmp_result = (x / 100) * y;
   tmp_result = tmp_result.toFixed(2);
@@ -48,25 +41,7 @@ const bill_calculation = (x, y, z) => {
   tip_total_amount.innerHTML = tmp_result * z;
 };
 
-
-/* const values = [];
-    
-    get_bill.addEventListener('change', ({ target }) => {
-        // const bill_value = target.value;
-        values.push(target.value);
-        // console.log(values);
-    })
-
-    
-    get_people.addEventListener('change', ({target}) => {
-        // const people_value = target.value;
-        values.push(target.value);
-        // console.log(values)
-    }) */
-
-
 /* 
-09076969865
     Get the bill, take the percentage of the bill according to any of the buttons clicked ie, 15%, 20% etc. and divide it by
     the number of people specified by the user. the result without dividing by number of people is displayed
     just beside the text "tip amount" and the result after dividing is displayed beside the text "total / person"
